8ab63ec2f6fd588dedfe74fc78467599593e5330
hakibenita <hakibenita@gmail.com>
2014-12-05 19:35:26 +0200

    Use span instead of div for transclution
    The directive is  usually used at a highe level so
    to be less intrusive we  better use an element without disaply:block by default.

--- massautocomplete.js
+++ massautocomplete.js
@@ -7,7 +7,7 @@ angular.module('MassAutoComplete', [])
     scope: { options: '&massAutocomplete' },
     transclude: true,
     template:
-      '<div ng-transclude></div>' +
+      '<span ng-transclude></span>' +
       '<div class="ac-container" ng-show="show_autocomplete && results.length > 0" style="position:absolute;">' +
       '  <ul class="ac-menu">' +
       '    <li ng-repeat="result in results" ng-if="$index > 0"' +
