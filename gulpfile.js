98718d8146ab23af813310260dc89b9e32e92ccf
Ran Benita <ran234@gmail.com>
2016-07-23 15:24:03 +0300

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    eslint = require('gulp-eslint');
 
-gulp.task('dist', function() {
+gulp.task('lint', function() {
+  return gulp.src('massautocomplete.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('dist', ['lint'], function() {
   return gulp.src('massautocomplete.js')
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
